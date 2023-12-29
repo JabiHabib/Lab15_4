300930ae6045b50381f5c533ff6615153bab9573
JabibHabib <kubusalsa@gmail.com>
2023-12-29 14:23:25 +0200

    Добавьте файлы проекта.

---
+++ Lab15_4/Lab15_4.cpp
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <locale>
+
+#define SIZE 10
+
+int main() {
+    setlocale(LC_ALL, "Ukr");
+    srand(time(NULL));
+
+    int x[SIZE];
+
+    for (int i = 0; i < SIZE; i++) {
+        x[i] = rand() % 100;
+    }
+
+    printf("Елементи масиву: ");
+    for (int i = 0; i < SIZE; i++) {
+        printf("%d ", x[i]);
+    }
+
+    int sum = 0;
+    for (int i = 0; i < SIZE; i++) {
+        sum += x[i];
+    }
+
+    printf("\nСума елементiв масиву: %d\n", sum);
+
+    return 0;
+}
